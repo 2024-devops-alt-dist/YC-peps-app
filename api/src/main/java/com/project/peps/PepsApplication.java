ddc7754a429c4bc551ba5d8a8aca8237e812ee9c
Leigh <psychotropes661@gmail.com>
2025-09-15 15:01:52 +0200

    init repo

---
+++ api/src/main/java/com/project/peps/PepsApplication.java
@@ -0,0 +1,13 @@
+package com.project.peps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepsApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/project/peps/PepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.peps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
