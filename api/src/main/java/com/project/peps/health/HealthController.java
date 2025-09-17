2cec053bed275d5d1289618dc9d01ecc44165a4d
Leigh <psychotropes661@gmail.com>
2025-09-17 11:51:01 +0200

    🩺 BACK (chore) Endpoint d'healthcheck pour connexion API-BDD

---
+++ api/src/main/java/com/project/peps/health/HealthController.java
@@ -0,0 +1,21 @@
+package com.project.peps.health;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    private final HealthService healthService;
+
+    public HealthController(HealthService healthService) {
+        this.healthService = healthService;
+    }
+
+    @GetMapping("/api/health")
+    public ResponseEntity<Map<String, String>> checkHealth() {
+        return healthService.checkHealth();
+    }
+}
---
+++ api/src/main/java/com/project/peps/health/HealthService.java
@@ -0,0 +1,34 @@
+package com.project.peps.health;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HealthService {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public HealthService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // Service méthode qui renvoie le statut de santé de l'API (response entity pour renvoyer le bon code HTTP + message)
+    public ResponseEntity<Map<String, String>> checkHealth() {
+        try {
+            Integer one = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            if (one != null && one == 1) {
+                return ResponseEntity.ok(
+                        Map.of("status", "ok", "message", "API prout prout"));
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    Map.of("status", "error", "message", "Unexpected DB result"));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+                    Map.of("status", "error", "message", "Database connection failed"));
+        }
+    }
+}
