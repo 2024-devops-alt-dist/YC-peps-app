b417281549e9371f312882805808602ff8f3f5cc
Leigh <psychotropes661@gmail.com>
2025-09-18 15:58:02 +0200

    🔧CLIENT (fix) MàJ hotreload pour Client

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -5,8 +6,26 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('client');
+  public apiState = signal('client');
+  public API_URL : string = 'http://localhost:8080';
+
+  // constructor(
+  //   private http: HttpClient,
+  // ) {}
+
+  // tryConnexion() {
+  //   this.http.get<Map<string, string>>('/api/health').subscribe({
+  //     next: (data) => {
+  //       this.apiState.set('API is healthy');
+  //       console.log('API is healthy:', data);
+  //     },
+  //     error: (error) => {
+  //       this.apiState.set('API is not reachable');
+  //       console.error('API is not reachable:', error);
+  //     }
+  //   });
+  // }
 }
